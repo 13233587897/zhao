e1b83cc99798f9bea075b29d7b593088c7d51ae4
mmy <1287887794@qq.com>
2019-07-16 08:52:25 +0800

    第九天考试

---
+++ demo9.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/16
+ * Time: 8:44
+ */
+
+$array = ['3,32,321'];
+function ll($n){
+    for ($i=0;$i<count();$i++){
+        $i=array();
+    }else{
+        $n=array();
+    }
+}
+
+
+?>
