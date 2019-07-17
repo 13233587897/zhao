ebede3df8e3cb921620eb072922ad701dd7b79a4
mmy <1287887794@qq.com>
2019-07-17 08:51:41 +0800

    第十天考试

---
+++ demo10.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/17
+ * Time: 8:46
+ */
+$arr = [2,4,3,6,3,2,5,5];
+
+function get($n,$m){
+    for ($i=1;$i<=count();$i++){
+        $n[]=$i;
+    }else{
+        $m[]=$i;
+    }
+}
+
+?>
