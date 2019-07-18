f6676cde1f3f5722ed547bcbc5c691457e806590
mmy <1287887794@qq.com>
2019-07-18 08:53:29 +0800

    第十一天考试

---
+++ demo11.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/18
+ * Time: 8:45
+ */
+$Arr=["student a am I"];
+
+function stu(){
+    for ($i=0;$i<=count();$i++){
+        $i = $i;
+    }
+}
+
+
+?>
