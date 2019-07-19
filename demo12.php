35c3398d99e70b9e6d4c478d1389a01267109be3
mmy <1287887794@qq.com>
2019-07-19 08:51:26 +0800

    第十二天考试

---
+++ demo12.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/19
+ * Time: 8:47
+ */
+$arr=[1,2,3,4,5,6,7,n];
+
+function lasa (){
+    for($i=1;$i<=count();$i++){
+        $i = [];
+    }
+}
+?>
