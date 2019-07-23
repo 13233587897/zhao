b1a05d73064bf0248bf32b37f18b83c3afe5702a
mmy <1287887794@qq.com>
2019-07-23 08:55:06 +0800

    第十五天考试

---
+++ demo15.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/23
+ * Time: 8:44
+ */
+function ll($i){
+    for($i<0;$i<=10000;$i++){
+        $i=0;
+    }else{
+        $i=0;
+    }
+}
+
+
+?>
