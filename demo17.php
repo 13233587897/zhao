3cd8667b003782c4b5aa60a01b4b0896a363ff45
mmy <1287887794@qq.com>
2019-07-25 08:56:46 +0800

    第十七天考试

---
+++ demo17.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/25
+ * Time: 8:46
+ */
+$arr_A = [1,3,6,9,];
+$arr_B = [2,4,5,8,20];
+function ll(){
+    for ($i=0;$i<=combine();$i++){
+        Combine($arr_A,$arr_B);
+    }
+
+}
+
+?>
