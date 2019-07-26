360a4bfa9629a12e33073ae434dd969aed586f4e
mmy <1287887794@qq.com>
2019-07-26 08:48:41 +0800

    第十八天日考

---
+++ demo18.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 赵谦
+ * Date: 2019/7/26
+ * Time: 8:41
+ */
+$n = 10;
+$number0f1($n);
+
+function get($n){
+    for ($i=0;$i>=$number0f1;$i++);
+    if ($number0f1){
+        $n=1;
+    }else{
+        $n=2;
+    }
+    return $n;
+}
+
+?>
